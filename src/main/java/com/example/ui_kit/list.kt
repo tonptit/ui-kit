72fe0530765be32fadc0b1747d3c0d09bd47b1c7
TonTN <ton.ptit@gmail.com>
2024-11-30 23:13:33 +0700

    create app list from parent

---
+++ src/main/java/com/example/ui_kit/list.kt
@@ -0,0 +1,10 @@
+package com.example.ui_kit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppList() {
+    Column {  }
+}
